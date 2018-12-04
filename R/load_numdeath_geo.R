97a84e9af9da0e7a14852eb32f455f599c01aa96
casey <casey@alis-imac.attlocal.net>
2018-12-03 20:27:50 -0800

    comments for load_numdeath_geo

--- R/load_numdeath_geo.R
+++ R/load_numdeath_geo.R
@@ -13,32 +13,31 @@ load_numdeath_geo <- function(numdeath, ssn_state_codes = "/home/ipums/casey-ipu
 
   numdeath[,"ssn_3" := as.numeric(substr(ssn, 1, 3))]
 
-  # read in state social security codes
+  ## read in state social security codes
   ssn_state_codes <- fread(ssn_state_codes)
 
-  # set merge keys
+  ## set merge keys
   setkey(numdeath, ssn_3)
   setkey(ssn_state_codes, ssn_3)
 
-
-  # merge on unique keys
+  ## merge on unique keys
   numdeath <- merge(numdeath, ssn_state_codes, on = key, all.x = TRUE)
   numdeath[,ssn_3:=NULL]
 
   cat("State from ZIP codes complete")
 
-  # create zip codes
+  ## create zip codes
   numdeath[,"zip_code_5" := as.character(substr(zip_residence, 1, 5))]
 
-  # read in zip code dataset
+  ## read in zip code dataset
   zip_code <- fread(zip_code_states, colClasses = list(character= 'zip_code_5'))
 
-  # set merge keys
+  ## set merge keys
   setkey(zip_code, zip_code_5)
   setkey(numdeath, zip_code_5)
 
 
-  # merge on unique keys
+  ## merge on unique keys
   numdeath <- merge(numdeath, zip_code, on = zip_code_5, all.x = TRUE)
 
   cat("City and State from ZIP codes complete\n")
