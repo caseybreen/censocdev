e92fddc954f647cf59f8e1d62674456c87b5fc43
Casey Breen <casey-ipums@demog.berkeley.edu>
2019-05-24 10:10:04 -0700

    update functions

--- R/merge_numdeath_numapp.R
+++ R/merge_numdeath_numapp.R
@@ -6,17 +6,21 @@
 #' @import data.table
 #' @export
 #'
-merge_numapp_numdeath <- function(numapp = numapp, numdeath = numdeath) {
+merge_numapp_numdeath <- function(numapp = numapp, numdeath = numdeath, census_year = 1940) {
 
   numapp <- numapp
   numdeath <- numdeath[, c("ssn", "zip_residence", "dyear", "dmonth", "byear_death_file", "bmonth_death_file", "dstate", "socstate"), with = FALSE]
   ss5 <- merge(numdeath, numapp, by = "ssn")
 
   ss5[,"census_age" := ifelse(bmonth < 4,
-                               1940 - byear,
-                               1939 - byear)]
+                              census_year - byear,
+                              census_year-1 - byear)]
+  
+  ## filter out people born after census year
+  ss5 <- ss5[census_age >= 0]
+  
 
-  ## Create two sets of linking keys (married name = lname, maiden name = father_lname)
+    ## Create two sets of linking keys (married name = lname, maiden name = father_lname)
   ss5[,"linking_key_married" := paste(lname, fname, census_age, bpl, sep = "_")]
   ss5[,"linking_key_married" := clean_key(linking_key_married),]
 
